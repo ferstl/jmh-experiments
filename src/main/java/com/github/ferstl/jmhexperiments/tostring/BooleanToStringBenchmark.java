73515da1d9caaf593f4e33f21e435aa9c1fbd249
Philippe Marschall <philippe.marschall@gmail.com>
2015-02-23 21:41:02 +0100

    Add Boolean toString Benchmarks
    Add benchmarks that test various ways to convert booleans to strings.

---
+++ src/main/java/com/github/ferstl/jmhexperiments/tostring/BooleanToStringBenchmark.java
@@ -0,0 +1,42 @@
+package com.github.ferstl.jmhexperiments.tostring;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.results.format.ResultFormatType;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import com.github.ferstl.jmhexperiments.ChartFucker;
+
+/**
+ * Show the differences between various toString methods for booleans.
+ */
+public class BooleanToStringBenchmark {
+  
+  public static void main(String[] args) throws RunnerException {
+    String fileName = "boolean-to-string-result.csv";
+    Options options = new OptionsBuilder()
+      .include(".*BooleanToStringBenchmark.*")
+      .warmupIterations(10)
+      .measurementIterations(10)
+      .resultFormat(ResultFormatType.CSV)
+      .result(fileName)
+      .build();
+    new Runner(options).run();
+
+    ChartFucker.fuck(options.getResult().orElse(fileName));
+  }
+
+  @Benchmark
+  public void instanceToString(Blackhole bh) {
+    bh.consume(Boolean.TRUE.toString());
+  }
+
+  @Benchmark
+  public void staticToString(Blackhole bh) {
+    bh.consume(Boolean.toString(true));
+  }
+
+}
